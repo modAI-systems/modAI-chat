6ca6bd0a4a67859441f3cfd3312ccf9e4b2994d5
guenhter <grill.guenther@gmail.com>
2025-09-10 18:47:25 +0200

    refact: remove dummy settings module

--- frontend_omni/src/services/builtInModules.ts
+++ frontend_omni/src/services/builtInModules.ts
@@ -2,7 +2,6 @@ import { moduleManager } from '../services/moduleManager'
 
 // Import built-in modules
 import { createModule as createChatModule } from '../components/modules/chat/Module'
-import { createModule as createUserSettingsModule } from '../components/modules/userSettings/Module'
 import { createModule as createUserProfileItemModule } from '../components/modules/userProfileItem/Module'
 import { createModule as createLoginPageModule } from '../components/modules/login/Module'
 
@@ -10,7 +9,6 @@ import { createModule as createLoginPageModule } from '../components/modules/log
 export function registerBuiltInModules() {
     const allModules = [
         createChatModule(),
-        createUserSettingsModule(),
         createLoginPageModule(),
         createUserProfileItemModule(),
     ]
