35ac05b97a15fffd0ab890cf33f7fab831e9c676
guenhter <grill.guenther@gmail.com>
2025-09-10 21:40:36 +0200

    refactor: rename package

--- frontend_omni/src/services/builtInModules.ts
+++ frontend_omni/src/services/builtInModules.ts
@@ -2,7 +2,7 @@ import { moduleManager } from '../services/moduleManager'
 
 // Import built-in modules
 import { createModule as createChatModule } from '../components/modules/chat/Module'
-import { createModule as createUserProfileItemModule } from '../components/modules/userProfileItem/Module'
+import { createModule as createUserProfileItemModule } from '../components/modules/user-profile-item/Module'
 import { createModule as createLoginPageModule } from '../components/modules/login/Module'
 
 
