405c9150227eb6df50181da369666309bc8a6a0a
guenhter <grill.guenther@gmail.com>
2025-09-13 11:29:54 +0200

    feat: don't show chat icon if logged out

--- frontend_omni/src/components/modules/chat/SidebarItem.tsx
+++ frontend_omni/src/components/modules/chat/SidebarItem.tsx
@@ -2,8 +2,15 @@ import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 import { Metadata, path, title } from "./Metadata";
+import { useSession } from "@/services/modules/session/ContextProvider";
 
 export function SidebarItem() {
+    const { session } = useSession();
+
+    if (!session) {
+        return null; // Don't render the item if the user is not authenticated
+    }
+
     return <SidebarMenuItem key={Metadata.id}>
         <SidebarMenuButton asChild tooltip={title}>
             <Link to={`${path}`}>
