1c6436bfe39c6cb6804087ced29322eb5139371c
DavidSnowfarmer <david@snowfarmer.dev>
2026-04-03 10:27:32 +0200

    add mobile e2e tests

--- e2e_tests/tests_omni_full/playwright.config.ts
+++ e2e_tests/tests_omni_full/playwright.config.ts
@@ -29,6 +29,14 @@ export default defineConfig({
             name: "webkit",
             use: { ...devices["Desktop Safari"] },
         },
+        {
+            name: "mobile-chrome",
+            use: { ...devices["Pixel 7"] },
+        },
+        {
+            name: "mobile-safari",
+            use: { ...devices["iPhone 14"] },
+        },
     ],
     webServer: [
         {
